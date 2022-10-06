d7e4e587a9ecac32d0fcd3d1db24bc692bef303d
mhshaker <mhshaker71@gmail.com>
2022-10-06 14:24:02 +0200

    results from 16.09.22

--- EpisodicModelUpdate.py
+++ EpisodicModelUpdate.py
@@ -14,36 +14,70 @@ import Uncertainty as unc
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.metrics import matthews_corrcoef
-
+from sklearn.metrics import log_loss
+from sklearn.datasets import make_classification
+import math
 
 # Parameters
-episodes_p =  0.05
-seed = 1
-runs = 5
-
+episodes_p =  0.01
+runs = 10
+data_name = "sim"
+n_samples = 50000
+n_classes = 5
 ########################################################### load data
 
-# features_all, targets_all   = dp.load_data("./Data/")
-# data = np.concatenate((features_all,targets_all), axis=1)
+if data_name == "amazon":
+    features_all, targets_all   = dp.load_data("./Data/")
+    data = np.concatenate((features_all,targets_all), axis=1)
+elif data_name == "amazon_small":
+    with open('Data/amazon_small.npy', 'rb') as f:
+        data = np.load(f)
 
-with open('Data/amazon_small.npy', 'rb') as f:
-    data = np.load(f)
+########################################################### metrics
 
-# data = pd.read_csv('Data/spambase.csv')
-# data = data.sample(frac=1)
-
-stream = DataStream(data)
-stream_length = stream.n_remaining_samples()
-episode_size = int(stream_length * episodes_p)
+def measures(model, x_ep, y_ep):
+    mcc = matthews_corrcoef(y_ep, model.predict(x_ep))
+    acc = model.score(x_ep, y_ep)
+    loss = log_loss(y_ep,model.predict_proba(x_ep))
+    return [acc, loss, mcc]
 
 stream_score_unc_list = []
 stream_score_score_list = []
 stream_score_all_list = []
+stream_score_nou_list = []
+
+stream_count_unc_list = []
+stream_count_score_list = []
+
 
 mode = "run all"
 
 for seed in range(runs):
     print("------------------------------------ run ", seed)
+
+    if data_name == "sim": ########################## sim data
+        x_n, y_n = make_classification(n_samples=n_samples * 2, n_features=500, n_informative=300, n_redundant=5, 
+                                n_repeated=0, n_classes=n_classes, n_clusters_per_class=1, weights=None, 
+                                flip_y=0.01, class_sep=1.0, hypercube=True, shift=0.0, scale=1.0, 
+                                shuffle=True, random_state=seed)
+
+        x_d, y_d = make_classification(n_samples=n_samples,     n_features=500, n_informative=300, n_redundant=15, 
+                                n_repeated=0, n_classes=n_classes, n_clusters_per_class=1, weights=None, 
+                                flip_y=0.01, class_sep=1.0, hypercube=True, shift=0.0, scale=1.0, 
+                                shuffle=True, random_state=seed+1)
+
+        x_ndn = np.concatenate((x_n[:n_samples], x_d, x_n[n_samples:]), axis=0)
+        y_ndn = np.concatenate((y_n[:n_samples], y_d, y_n[n_samples:]), axis=0)
+        data = np.concatenate((x_ndn,y_ndn.reshape(-1,1)), axis=1) 
+
+    stream = DataStream(data)
+    stream_length = stream.n_remaining_samples()
+    episode_size = int(stream_length * episodes_p)
+
+    if data_name == "sim":
+        drift_episode1 = math.ceil(n_samples/episode_size)
+        drift_episode2 = math.ceil(n_samples*2/episode_size)
+
     # defining the model
     if mode == "unc" or mode == "run all":
         stream.restart()
@@ -57,7 +91,7 @@ for seed in range(runs):
         tu_set = tu.mean()
         # Uncertainty detection
         # episode loop
-        updatecounter = 0
+        updatecounter_unc = 0
         episode = 0
         stream_score_unc = []
         while True:
@@ -67,9 +101,10 @@ for seed in range(runs):
                 break
             episode +=1
 
-            s = matthews_corrcoef(y_ep, model.predict(x_ep))
+            # s = matthews_corrcoef(y_ep, model.predict(x_ep))
             # s = model.score(x_ep, y_ep)
-            stream_score_unc.append(s)
+            m = measures(model, x_ep, y_ep)
+            stream_score_unc.append(m)
 
             tu, eu, au = unc.model_uncertainty(model, x_ep, x_train, y_train, laplace_smoothing=1)
             tu_ep = tu.mean()
@@ -77,18 +112,24 @@ for seed in range(runs):
                 x_train, y_train = x_ep, y_ep
                 model = RandomForestClassifier(max_depth=10, n_estimators=10, random_state=seed)
                 model.fit(x_ep, y_ep) # remove keys when fiting the model
-                updatecounter +=1
+                updatecounter_unc +=1
             #     print("episode ", episode, "D")
             # else:
             #     print("episode ", episode)
 
             # train the model / update
         stream_score_unc_list.append(stream_score_unc)
-        print("unc update count", updatecounter)
+        print("unc update count", updatecounter_unc)
 
         unc_save = np.array(stream_score_unc_list)
+        print(unc_save.shape)
         with open('results/stream_score_unc_list.npy', 'wb') as f:
             np.save(f, unc_save)
+
+        stream_count_unc_list.append(updatecounter_unc)
+        unc_save_c = np.array(stream_count_unc_list)
+        with open('results/stream_count_unc_list.npy', 'wb') as f:
+            np.save(f, unc_save_c)
 
     if mode == "err" or mode == "run all":
 
@@ -113,30 +154,30 @@ for seed in range(runs):
                 break
             episode +=1
 
-            s = matthews_corrcoef(y_ep, model.predict(x_ep))
+            # s = matthews_corrcoef(y_ep, model.predict(x_ep))
             # s = model.score(x_ep, y_ep)
-            stream_score_score.append(s)
-
-            if s > score_set:
-                update_model = True
-            #     print("episode ", episode, "D")
-            # else:
-            #     print("episode ", episode)
-
+            m = measures(model, x_ep, y_ep)
+            stream_score_score.append(m)
 
             # train the model / update
-            if update_model:
+            if m[2] < score_set: # score_set is MCC - higher is better
                 x_train, y_train = x_ep, y_ep
                 model = RandomForestClassifier(max_depth=10, n_estimators=10, random_state=seed)
                 model.fit(x_ep, y_ep) # remove keys when fiting the model
                 update_model = False
                 updatecounter_score +=1
+
         stream_score_score_list.append(stream_score_score)
         print("score update count", updatecounter_score)
 
         score_save = np.array(stream_score_score_list)
         with open('results/stream_score_score_list.npy', 'wb') as f:
             np.save(f, score_save)
+
+        stream_count_score_list.append(updatecounter_score)
+        score_save_c = np.array(stream_count_score_list)
+        with open('results/stream_count_score_list.npy', 'wb') as f:
+            np.save(f, score_save_c)
 
     if mode == "all" or mode == "run all":
         # always update
@@ -148,7 +189,6 @@ for seed in range(runs):
         _, _ = stream.next_sample(episode_size * 2)
 
         # episode loop
-        update_model = True
         updatecounter_all = 0
         episode = 0
         stream_score_all = []
@@ -160,15 +200,16 @@ for seed in range(runs):
                 break
             episode +=1
 
-            s = matthews_corrcoef(y_ep, model.predict(x_ep))
-            stream_score_all.append(s)
+            # s = matthews_corrcoef(y_ep, model.predict(x_ep))
+            m = measures(model, x_ep, y_ep)
+            stream_score_all.append(m)
 
             # train the model / update
-            if update_model:
-                x_train, y_train = x_ep, y_ep
-                model = RandomForestClassifier(max_depth=10, n_estimators=10, random_state=seed)
-                model.fit(x_ep, y_ep) # remove keys when fiting the model
-                updatecounter_all +=1
+            x_train, y_train = x_ep, y_ep
+            model = RandomForestClassifier(max_depth=10, n_estimators=10, random_state=seed)
+            model.fit(x_ep, y_ep) # remove keys when fiting the model
+            updatecounter_all +=1
+
         stream_score_all_list.append(stream_score_all)
         print("all update count", updatecounter_all)
 
@@ -176,7 +217,7 @@ for seed in range(runs):
         with open('results/stream_score_all_list.npy', 'wb') as f:
             np.save(f, all_save)
 
-    if mode == "no" or mode == "run all":
+    if mode == "nou" or mode == "run all":
         # always update
         stream.restart()
         model = RandomForestClassifier(max_depth=10, n_estimators=10, random_state=seed)
@@ -186,10 +227,9 @@ for seed in range(runs):
         _, _ = stream.next_sample(episode_size * 2)
 
         # episode loop
-        update_model = True
-        updatecounter_all = 0
+        updatecounter_no = 0
         episode = 0
-        stream_score_all = []
+        stream_score_nou = []
         while True:
             # print("episode ", episode)
             # setup new episode
@@ -198,12 +238,13 @@ for seed in range(runs):
                 break
             episode +=1
 
-            s = matthews_corrcoef(y_ep, model.predict(x_ep))
-            stream_score_all.append(s)
+            # s = matthews_corrcoef(y_ep, model.predict(x_ep))
+            m = measures(model, x_ep, y_ep)
+            stream_score_nou.append(m)
 
-        stream_score_all_list.append(stream_score_all)
-        print("all update count", updatecounter_all)
+        stream_score_nou_list.append(stream_score_nou)
+        print("all update count", updatecounter_no)
 
-        all_save = np.array(stream_score_all_list)
+        nou_save = np.array(stream_score_nou_list)
         with open('results/stream_score_no_list.npy', 'wb') as f:
-            np.save(f, all_save)
+            np.save(f, nou_save)
--- plot_update.py
+++ plot_update.py
@@ -2,27 +2,88 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-with open('results/stream_score_unc_list.npy', 'rb') as f:
+run_name = "sim data 50d 5c run10/"
+# run_name = ""
+run_name = "amazon data/"
+
+with open(f"results/{run_name}stream_score_unc_list.npy", 'rb') as f:
     unc = np.load(f)
-with open('results/stream_score_unc_list.npy', 'rb') as f:
+with open(f"results/{run_name}stream_score_score_list.npy", 'rb') as f:
     err = np.load(f)
-with open('results/stream_score_unc_list.npy', 'rb') as f:
+with open(f"results/{run_name}stream_score_all_list.npy", 'rb') as f:
     all = np.load(f)
-with open('results/stream_score_no_list.npy', 'rb') as f:
+with open(f"results/{run_name}stream_score_no_list.npy", 'rb') as f:
     nou = np.load(f)
 
+
+with open(f"results/{run_name}stream_count_unc_list.npy", 'rb') as f:
+    unc_c = np.load(f)
+
+with open(f"results/{run_name}stream_count_score_list.npy", 'rb') as f:
+    err_c = np.load(f)
+
 print(unc.shape)
-unc = unc.mean(axis=0)
-err = err.mean(axis=0)
-all = all.mean(axis=0)
-nou = nou.mean(axis=0)
+unc_c = int(unc_c.mean())
+err_c = int(err_c.mean())
+
+unc = unc.mean(axis=0)[:-2]
+err = err.mean(axis=0)[:-2]
+all = all.mean(axis=0)[:-2]
+nou = nou.mean(axis=0)[:-2]
+
+print(unc.shape)
 # unc = unc[0,:]
 # err = err[0,:]
 # all = all[0,:]
 
-plt.plot(nou, label="nou")
-plt.plot(unc, label="unc")
-plt.plot(err, label="error")
-plt.plot(all, label="all")
+plt.plot(nou, label="nou (0)", alpha=0.8)
+plt.plot(err, label=f"error ({err_c})", alpha=0.8)
+plt.plot(unc, label=f"unc ({unc_c})", c="black", alpha=0.8)
+plt.plot(all, label=f"all ({len(all)})", alpha=0.8)
+
 plt.legend()
-plt.savefig("results/plot.png")
+plt.xlabel("Episodes")
+plt.ylabel("Matthews correlation coefficient (MCC)")
+plt.savefig(f"results/{run_name}_MCC.png")
+plt.close()
+
+exit()
+
+plt.plot(nou[:,0], label="nou (0)", alpha=0.8) # , '--'
+plt.plot(err[:,0], label=f"error ({err_c})", alpha=0.8)
+plt.plot(unc[:,0], label=f"unc ({unc_c})", c="black", alpha=0.8)
+plt.plot(all[:,0], label=f"all ({len(all)})", alpha=0.8)
+plt.vlines(x=31, ymin=nou[:,0].min() - 0.1 * all[:,0].max(), ymax=all[:,0].max() + 0.1 * all[:,0].max(), color='gray', alpha=0.5)
+plt.vlines(x=64, ymin=nou[:,0].min() - 0.1 * all[:,0].max(), ymax=all[:,0].max() + 0.1 * all[:,0].max(), color='gray', alpha=0.5)
+
+plt.legend()
+plt.xlabel("Episodes")
+plt.ylabel("Accuracy")
+plt.savefig(f"results/{run_name}_ACC.png")
+plt.close()
+
+plt.plot(nou[:,1], label="nou (0)", alpha=0.8)
+plt.plot(err[:,1], label=f"error ({err_c})", alpha=0.8)
+plt.plot(unc[:,1], label=f"unc ({unc_c})", c="black", alpha=0.8)
+plt.plot(all[:,1], label=f"all ({len(all)})", alpha=0.8)
+plt.vlines(x=31, ymin=nou[:,1].min() - 0.1 * all[:,1].max(), ymax=all[:,1].max() + 0.1 * all[:,1].max(), color='gray', alpha=0.5)
+plt.vlines(x=64, ymin=nou[:,1].min() - 0.1 * all[:,1].max(), ymax=all[:,1].max() + 0.1 * all[:,1].max(), color='gray', alpha=0.5)
+
+plt.legend()
+plt.xlabel("Episodes")
+plt.ylabel("Log_loss")
+plt.savefig(f"results/{run_name}_Loss.png")
+plt.close()
+
+plt.plot(nou[:,2], label="nou (0)", alpha=0.8)
+plt.plot(err[:,2], label=f"error ({err_c})", alpha=0.8)
+plt.plot(unc[:,2], label=f"unc ({unc_c})", c="black", alpha=0.8)
+plt.plot(all[:,2], label=f"all ({len(all)})", alpha=0.8)
+plt.vlines(x=31, ymin=nou[:,2].min() - 0.1 * all[:,2].max(), ymax=all[:,2].max() + 0.1 * all[:,2].max(), color='gray', alpha=0.5)
+plt.vlines(x=64, ymin=nou[:,2].min() - 0.1 * all[:,2].max(), ymax=all[:,2].max() + 0.1 * all[:,2].max(), color='gray', alpha=0.5)
+
+plt.legend()
+plt.xlabel("Episodes")
+plt.ylabel("Matthews correlation coefficient (MCC)")
+plt.savefig(f"results/{run_name}_MCC.png")
+plt.close()
